fd9c5e993973250347eba1e5085b92d0a32255be
Tom Hartnett <tomhartnett@gmail.com>
2025-03-27 08:57:54 -0500

    Basic, naive URL encoding and decoding.

--- Thelp/ContentView.swift
+++ Thelp/ContentView.swift
@@ -5,17 +5,56 @@
 //  Created by Tom Hartnett on 3/27/25.
 //
 
+import Foundation
 import SwiftUI
 
 struct ContentView: View {
+    @State var encoded = ""
+    @State var decoded = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextEditor(text: $encoded)
+
+            HStack {
+                Button(action: {
+                    decoded = encoded.removingPercentEncoding ?? ""
+                }) {
+                    Text("↓ Decode")
+                }
+
+                Button(action: {
+                    encoded = ""
+                    decoded = ""
+                }) {
+                    Text("Clear")
+                }
+
+                Button(action: {
+                    let allowedCharacters = CharacterSet.urlQueryAllowed
+                        .union(.urlPathAllowed)
+                        .union(.urlHostAllowed)
+                        .union(.urlFragmentAllowed)
+                        .union(.urlUserAllowed)
+                        .union(.urlPasswordAllowed)
+
+                    encoded = decoded.addingPercentEncoding(withAllowedCharacters: allowedCharacters) ?? ""
+                }) {
+                    Text("Encode ↑")
+                }
+            }
+
+            TextEditor(text: $decoded)
         }
         .padding()
+    }
+
+    func decode() {
+        
+    }
+
+    func encode() {
+
     }
 }
 
