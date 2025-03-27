582307bbde16a0befd2470837cd29b2805e9bd83
Tom Hartnett <tomhartnett@gmail.com>
2025-03-27 08:37:07 -0500

    Initial Commit

---
+++ Thelp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thelp
+//
+//  Created by Tom Hartnett on 3/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thelp/ThelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThelpApp.swift
+//  Thelp
+//
+//  Created by Tom Hartnett on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ThelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
