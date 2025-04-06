3ddb154d7281128bcf562b865fb3d0cf60f5e306
Tom Hartnett <tomhartnett@gmail.com>
2025-04-06 14:49:37 -0500

    Added copy and clear buttons for each text field.

--- Thelp/Views/CodecView.swift
+++ Thelp/Views/CodecView.swift
@@ -7,36 +7,92 @@
 
 import SwiftUI
 
+enum Field: Hashable {
+    case encodedText
+    case decodedText
+}
+
 struct CodecView: View {
     @Binding var viewModel: ViewModel
 
+    @FocusState private var focusedField: Field?
+
     var body: some View {
         VStack {
+            HStack(spacing: 8) {
+                Spacer()
+
+                Button(action: {
+                    let pasteboard = NSPasteboard.general
+                    pasteboard.clearContents()
+                    pasteboard.setString(viewModel.encoded, forType: .string)
+                }) {
+                    Image(systemName: "clipboard")
+                }
+
+                Button(role: .destructive, action: {
+                    viewModel.encoded = ""
+                }) {
+                    Image(systemName: "clear")
+                }
+            }
+            .buttonStyle(PlainButtonStyle())
+            .padding(.trailing, 12)
+
             TextEditor(text: $viewModel.encoded)
                 .monospaced()
+                .focused($focusedField, equals: Field.encodedText)
 
             HStack {
-                Button(action: {
-                    viewModel.decode()
-                }) {
-                    Text("↓ Decode")
-                }
+                HStack {
+                    Button(action: {
+                        viewModel.decode()
+                    }) {
+                        Text("↓ Decode")
+                    }
 
-                Button(action: {
-                    viewModel.clearAll()
-                }) {
-                    Text("Clear")
-                }
+                    Button(action: {
+                        viewModel.encode()
+                    }) {
+                        Text("Encode ↑")
+                    }
 
-                Button(action: {
-                    viewModel.encode()
-                }) {
-                    Text("Encode ↑")
+                    Button(action: {
+                        viewModel.clearAll()
+                    }) {
+                        Text("Clear All")
+                    }
                 }
+                .buttonStyle(AccessoryBarActionButtonStyle())
+                .padding(.leading, 12)
+                .padding(.vertical, 2)
+
+                Spacer()
+
+                HStack {
+                    Button(action: {
+                        let pasteboard = NSPasteboard.general
+                        pasteboard.clearContents()
+                        pasteboard.setString(viewModel.decoded, forType: .string)
+                    }) {
+                        Image(systemName: "clipboard")
+                    }
+
+                    Button(role: .destructive, action: {
+                        viewModel.decoded = ""
+                    }) {
+                        Image(systemName: "clear")
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                .padding(.trailing, 12)
             }
 
             TextEditor(text: $viewModel.decoded)
                 .monospaced()
+        }
+        .onAppear {
+            focusedField = .encodedText
         }
     }
 }
