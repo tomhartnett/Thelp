3474aff5c5dbdffc961b6d47b87118b5f95a0b97
Tom Hartnett <tomhartnett@gmail.com>
2025-05-26 08:22:55 -0500

    Added bounce animation to buttons in ToolbarView.

--- Thelp/Views/ToolbarView.swift
+++ Thelp/Views/ToolbarView.swift
@@ -10,9 +10,23 @@ import SwiftUI
 struct ToolbarView: View {
     @Binding var text: String
 
+    @State private var copyButtonScale = 1.0
+
+    @State private var trashButtonScale = 1.0
+
     var body: some View {
         HStack {
             Button(action: {
+                withAnimation(.bouncy) {
+                    copyButtonScale *= 1.1
+                }
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    withAnimation(.bouncy) {
+                        copyButtonScale = 1.0
+                    }
+                }
+
                 guard !text.isEmpty else { return }
 
                 let pasteboard = NSPasteboard.general
@@ -21,13 +35,29 @@ struct ToolbarView: View {
             }) {
                 Image(systemName: "clipboard")
             }
+            .scaleEffect(copyButtonScale)
+            .animation(.spring(duration: 0.25, bounce: 0.75), value: copyButtonScale)
+            .opacity(copyButtonScale > 1 ? 0.5 : 1)
 
             Button(role: .destructive, action: {
+                withAnimation(.bouncy) {
+                    trashButtonScale *= 1.1
+                }
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    withAnimation(.bouncy) {
+                        trashButtonScale = 1.0
+                    }
+                }
+
                 text = ""
             }) {
                 Image(systemName: "trash")
             }
             .foregroundStyle(Color(.appRed))
+            .scaleEffect(trashButtonScale)
+            .animation(.spring(duration: 0.25, bounce: 0.75), value: trashButtonScale)
+            .opacity(trashButtonScale > 1 ? 0.5 : 1)
         }
         .buttonStyle(PlainButtonStyle())
     }
